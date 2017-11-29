0005d449c87a543db3fab8e2c5f47f6f6ca0726e
fishlinghu <fishlinghu@gmail.com>
2017-11-29 03:46:25 -0500

    Finish ta part of the script, untested

--- script_robustness_fat_tree.py
+++ script_robustness_fat_tree.py
@@ -2,6 +2,7 @@
 import sys
 import os
 
+
 def main():
     # usage: $script.py N num_of_servers
     # N = number of switches
@@ -14,18 +15,38 @@ def main():
 
     # compute targeting attack
     filename_prefix = "ta_" + str(N) + "_" + str(num_of_servers) + "_"
-    for num_switch_remained in range(N, 0, -1):
-        filename = filename_prefix + str(num_switch_remained) + ".txt"
-        command = "./waf --run \"scratch/BisectionBW --input=scratch/" + filename + "\""
-        os.system(command)
+    for num_switch_removed in range(N, 0, -1):
+        filename = filename_prefix + str(num_switch_removed) + ".txt"
+
+        with open(filename, 'r') as input_file, open("edge_list", 'w') as output_file:
+            first_line_flag = True
+            for line in input_file:
+                if first_line_flag:
+                    # first line is the list of server ids
+                    with open("server_file", 'w') as server_file:
+                        server_file.write(line)
+                    server_file.close()
+                    first_line_flag = False
+                else:
+                    output_file.write(line)
+        input_file.close()
+        output_file.close()
+
+        server_file = ""
+        command = "./waf --run \"scratch/BisectionBW --input=edge_list --servers=server_file\""
+        # os.system(command)
         # print(command)
+        os.remove("edge_list")
+        os.remove("server_file")
 
     # compute randomly attack
+    """
     filename_prefix = "rf_" + str(N) + "_" + str(num_of_servers) + "_"
     for num_switch_remained in range(N, 0, -1):
         filename = filename_prefix + str(num_switch_remained) + ".txt"
         command = "./waf --run \"scratch/BisectionBW --input=scratch/" + filename + "\""
         os.system(command)
+    """
 
 if __name__ == "__main__":
     main()
